9455e71999f2ff4f698162267def35a3a9735510
Kris <912531455@qq.com>
2022-02-14 16:40:53 +0800

    firstIdeaCommitHotfix

---
+++ src/main/java/gittest/GitTestmain.java
@@ -0,0 +1,14 @@
+package gittest;
+
+/**
+ * @author ：Kris
+ * @date ：Created in 2022/2/14 16:39
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class GitTestmain {
+    public static void main(String[] args) {
+        System.out.println("更新git");
+    }
+}
